3f6f9c3732ba2c5615417db2f886f2ef3bdec69a
MatKapp <mka@bcsg.pl>
2019-10-25 21:18:21 +0200

    Key pressed event project

---
+++ ControllerBackend/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using WindowsInput;
+
+namespace ControllerBackend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Threading.Thread.Sleep(10000);
+            var simulator = new InputSimulator();
+            simulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.VK_E);
+        }
+    }
+}
