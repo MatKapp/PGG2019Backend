3aef146412558565dc642c5c3eba28a85f641f37
MatKapp <mka@bcsg.pl>
2019-10-25 22:36:51 +0200

    Communication first version

---
+++ CommunicationCore/CommunicationCore/GameCommunication.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketManager;
+using WebSocketManager.Common;
+
+namespace CommunicationCore
+{
+    public class GameCommunication : WebSocketHandler
+    {
+        public GameCommunication(WebSocketConnectionManager webSocketConnectionManager) : base(webSocketConnectionManager, new ControllerMethodInvocationStrategy())
+        {
+            ((ControllerMethodInvocationStrategy)MethodInvocationStrategy).Controller = this;
+        }
+
+        public override async Task OnConnected(WebSocket socket)
+        {
+            await base.OnConnected(socket);
+            var socketId = WebSocketConnectionManager.GetId(socket);
+
+            var message = new Message()
+            {
+                MessageType = MessageType.Text,
+                Data = $"{socketId} you are connected Peter"
+            };
+
+            await SendMessageAsync(socketId, message);
+        }
+    }
+}
---
+++ CommunicationCore/CommunicationCore/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebSocketManager;
+
+namespace CommunicationCore
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddWebSocketManager();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IServiceProvider serviceProvider)
+        {
+            app.UseWebSockets();
+            app.MapWebSocketManager("/game", serviceProvider.GetService<GameCommunication>());
+
+            app.UseStaticFiles();
+        }
+    }
+}
